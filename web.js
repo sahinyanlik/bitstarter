5bef9ca58c5ed7e4b7b3975f1d98f413b1cc83cf
sahinyanlik <sahinyanlik@gmail.com>
2013-07-07 10:38:50 +0000

    test

--- web.js
+++ web.js
@@ -1,9 +1,14 @@
 var express = require('express');
-
+var fs=require('fs');
+buf=new Buffer(256);
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World c2');
+ fs.readFile('index.html',function(err,data){
+  if(err) throw err;
+   response.write(data);
+});
+
 });
 
 var port = process.env.PORT || 5000;
